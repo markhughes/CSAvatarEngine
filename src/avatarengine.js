db13feb6101b1f81a04f5dbda2f5446f2040167b
Mark Hughes <mark@markeh.me>
2015-04-03 18:22:53 +1100

    Documenting and cleaning up

--- src/avatarengine.js
+++ src/avatarengine.js
@@ -45,52 +45,53 @@ var AvatarGender = { Male: "male", Female: "female" };
 
 // Main Object 
 var AvatarEngine = {
+	// Path to CSAvatarAssets
 	path: "../../CSAvatarAssets/",
+	
+	// These are tops without any arm graphics, add both the integer and the string
 	topsWithoutArms: [ 2, "2" ],
+	
+	// Returns an x-avatar element
 	get: function(id, gender) {
 		
-		if(gender == 0 || gender ==null || gender == "0") {
+		if(gender == 0 || gender == null || gender == undefined || gender == "0") {
 			gender = AvatarGender.Male;	
 		}
 		
-		var avatarElement = document.createElement('x-avatar')
+		// Crete element
+		var avatarElement = document.createElement("x-avatar");
 		
+		// Set default information
 		avatarElement.className = "v-avatar-" + id;
 		avatarElement.createdAt = new Date();
-		
 		avatarElement.avatarID = id;
-		
 		avatarElement.avatarGender = gender;
 		avatarElement.avatarDirection = AvatarDirection.SouthEast;
-		
-		avatarElement.set = function(data) {
-			if(data.gender != undefined) {
-				this.avatarGender = data.gender;	
-			}
-			
-			if(data.gender != undefined) {
-				this.avatarGender = data.gender;	
-			}			
-		}
 		avatarElement.avatarAt = 1;
 		avatarElement.isWalking = false;
 		avatarElement.isSitting = false;
 		
+		// Set default clothes
 		avatarElement.clothes = {
-			hair: 0,
-			head: "1",
-			body: "1",
-			legs: 0,
-			top: 0,
-			eyes: "1",
-			face: "1"
+			hair:	0,
+			head:	1,
+			body:	1,
+			legs:	0,
+			top:	0,
+			eyes:	1,
+			face:	1,
 		};
 		
+		/**
+		 * update()
+		 * Updates the avatar element with whatever we need
+		 */
 		avatarElement.update = function() {
 			this.innerHTML = "";
 			
 			avatarElement.avatarAt++;
 			
+			// Append our required elements to the element
 			this.appendChild(this.generateHair());
 			this.appendChild(this.generateHead());
 			this.appendChild(this.generateFace());
@@ -99,35 +100,47 @@ var AvatarEngine = {
 			this.appendChild(this.generateArmA());
 			this.appendChild(this.generateArmB());
 			
+			// Only generate top if we have one on
 			if(this.generateTop() != 0 && this.generateTop() != "0") {
 				this.appendChild(this.generateTop());
 			}
 			
+			// Only generate legs if we have one on
 			if(this.generateLegs() != 0 && this.generateLegs() != "0") {
 				this.appendChild(this.generateLegs());
 			}
 			
-			
+			// All graphics work through 4 different stages, so when we're at
+			// 4 we should revert to 1
 			if(avatarElement.avatarAt == 4) {
 				avatarElement.avatarAt = 1;	
 			}
 		};
 		
+		/**
+		 * generateHead()
+		 * Returns the x-avatar-head element
+		 */
 		avatarElement.generateHead = function() {
-			var a_body = document.createElement("x-avatar-head");
-			a_body.style.position = "absolute";
-			a_body.style.zIndex = "14";
+			var eHead = document.createElement("x-avatar-head");
+			eHead.style.position = "absolute";
+			eHead.style.zIndex = "14";
 			
-			a_body.innerHTML = "<img src=\""+AvatarEngine.path+"assets/head/"+this.avatarDirection+"/"+avatarElement.clothes.head+".png\">";
+			eHead.innerHTML = "<img src=\""+AvatarEngine.path+"assets/head/"+this.avatarDirection+"/"+avatarElement.clothes.head+".png\">";
 
-			return a_body;
+			return(eHead);
 		};
 		
+		/**
+		 * generateHair()
+		 * Returns the x-avatar-hair element
+		 */
 		avatarElement.generateHair = function() {
 			var eHair = document.createElement("x-avatar-hair");
 			eHair.style.position = "absolute";
 			eHair.style.zIndex = "16";
 			
+			// only add contents if they have hair 
 			if(avatarElement.clothes.hair != 0 && avatarElement.clothes.hair != "0") {
 				eHair.innerHTML = "<img src=\""+AvatarEngine.path+"assets/hair/"+avatarElement.clothes.hair+"/"+this.avatarDirection+".png\">";
 			}
@@ -135,9 +148,15 @@ var AvatarEngine = {
 			return eHair;
 		};
 		
+		/**
+		 * generateArmA()
+		 * Returns generateArmA x-avatar-arm-a element
+		 */
 		avatarElement.generateArmA = function() {
-			var a_body = document.createElement("x-avatar-arm-a");
+			var eArmA = document.createElement("x-avatar-arm-a");
 			var s = 1;
+			
+			// if we're walking, lets update the step
 			if(this.isWalking) {
 				if(this.avatarAt > 2) {
 					s = 1;
@@ -147,16 +166,24 @@ var AvatarEngine = {
 				
 				if(s == 1) { s = 2; } else { s = 1; }
 			}
-			a_body.style.position = "absolute";
-			a_body.style.zIndex = "9";
 			
-			a_body.innerHTML = "<img src=\""+AvatarEngine.path+"assets/body/shared/a/arm-"+s+".png\">";
+			eArmA.style.position = "absolute";
+			eArmA.style.zIndex = "9";
 			
-			return a_body;
+			eArmA.innerHTML = "<img src=\""+AvatarEngine.path+"assets/body/shared/a/arm-"+s+".png\">";
+			
+			return eArmA;
 		};
+		
+		/**
+		 * generateArmB()
+		 * Returns generateArmA x-avatar-arm-a element
+		 */
 		avatarElement.generateArmB = function() {
-			var a_body = document.createElement("x-avatar-arm-b");
+			var eArmB = document.createElement("x-avatar-arm-b");
 			var s = 1;
+			
+			// if we're walking, lets update the step (like in ArmA)
 			if(this.isWalking) {
 				if(this.avatarAt > 2) {
 					s = 1;
@@ -164,14 +191,18 @@ var AvatarEngine = {
 					s = this.avatarAt;	
 				}
 			}
-			a_body.style.position = "absolute";
-			a_body.style.zIndex = "14";
+			eArmB.style.position = "absolute";
+			eArmB.style.zIndex = "14";
 			
-			a_body.innerHTML = "<img src=\""+AvatarEngine.path+"assets/body/shared/b/arm-"+s+".png\">";
+			eArmB.innerHTML = "<img src=\""+AvatarEngine.path+"assets/body/shared/b/arm-"+s+".png\">";
 
-			return a_body;
+			return eArmB;
 		};
 		
+		/**
+		 * generateTop()
+		 * Returns generateArmA x-avatar-top element, with the top-armA and top-armB elements
+		 */
 		avatarElement.generateTop = function() {
 			
 			if(this.clothes.top == 0 || this.clothes.top == "0") {
@@ -183,8 +214,8 @@ var AvatarEngine = {
 			eTop.style.zIndex = "15";
 			
 			var mid = document.createElement("x-avatar-mid");
-			var armA = document.createElement("x-avatar-armA");
-			var armB = document.createElement("x-avatar-armB");
+			var armA = document.createElement("x-avatar-top-armA");
+			var armB = document.createElement("x-avatar-top-armB");
 			
 			mid.style.position = "absolute";
 			mid.style.zIndex = "15";
@@ -197,14 +228,13 @@ var AvatarEngine = {
 			
 			mid.innerHTML = "<img src=\""+AvatarEngine.path+"assets/top/"+this.clothes.top+"/top-se-stand.png\">";
 			
+			// If it has arms, add them
 			if(AvatarEngine.topsWithoutArms.indexOf(this.clothes.top) == -1) {
-			
-			console.log(this.clothes);
-			armA.innerHTML = "<img src=\""+AvatarEngine.path+"assets/top/"+this.clothes.top+"/arm-a-1.png\">";
-			
-			armB.innerHTML = "<img src=\""+AvatarEngine.path+"assets/top/"+this.clothes.top+"/arm-b-1.png\">";
-			
+				armA.innerHTML = "<img src=\""+AvatarEngine.path+"assets/top/"+this.clothes.top+"/arm-a-1.png\">";
+				armB.innerHTML = "<img src=\""+AvatarEngine.path+"assets/top/"+this.clothes.top+"/arm-b-1.png\">";
 			}
+			
+			// Put it all together and send it back! 
 			eTop.appendChild(mid);
 			eTop.appendChild(armA);
 			eTop.appendChild(armB);
@@ -213,10 +243,15 @@ var AvatarEngine = {
 			
 		};
 		
+		/**
+		 * generateFace()
+		 * Returns x-avatar-face 
+		 */
 		avatarElement.generateFace = function(face) {
 			if(face == undefined) {
 				face = this.clothes.face;
 			}
+			
 			var eFace = document.createElement("x-avatar-face");
 			eFace.style.position = "absolute";
 			eFace.style.zIndex = "15";
@@ -226,61 +261,83 @@ var AvatarEngine = {
 			return eFace;
 		};
 		
+		/**
+		 * generateEyes()
+		 * Returns x-avatar-eyes 
+		 */
 		avatarElement.generateEyes = function(eyes) {
 			if(eyes == undefined) {
 				eyes = this.clothes.eyes;
 			}
-			var eEye = document.createElement("x-avatar-eyes");
-			eEye.style.position = "absolute";
-			eEye.style.zIndex = "15";
 			
-			eEye.innerHTML = "<img src=\""+AvatarEngine.path+"assets/eyes/"+eyes+"/"+this.avatarDirection+".png\">";
+			var eEyes = document.createElement("x-avatar-eyes");
+			eEyes.style.position = "absolute";
+			eEyes.style.zIndex = "15";
+			
+			eEyes.innerHTML = "<img src=\""+AvatarEngine.path+"assets/eyes/"+eyes+"/"+this.avatarDirection+".png\">";
 
-			return eEye;
+			return eEyes;
 		};
 		
+		/**
+		 * generateBody()
+		 * Returns x-avatar-body 
+		 */
 		avatarElement.generateBody = function() {
-			var a_body = document.createElement("x-avatar-body");
-			a_body.style.position = "absolute";
-			a_body.style.zIndex = "10";
+			var eBody = document.createElement("x-avatar-body");
+			eBody.style.position = "absolute";
+			eBody.style.zIndex = "10";
 			
+			// If they're sitting, we'll show the sit asset
 			if(this.isSitting) {
-				a_body.innerHTML = "<img src=\""+AvatarEngine.path+"assets/body/"+this.avatarGender+"/body-se-sit.png\">";
-
+				eBody.innerHTML = "<img src=\""+AvatarEngine.path+"assets/body/"+this.avatarGender+"/body-se-sit.png\">";
 			} else {
 				if(this.isWalking) {
-					a_body.innerHTML = "<img src=\""+AvatarEngine.path+"assets/body/"+this.avatarGender+"/body-se-walk-"+this.avatarAt+".png\">";
+					// if they're walking, we'll show the walking assets (in steps)
+					eBody.innerHTML = "<img src=\""+AvatarEngine.path+"assets/body/"+this.avatarGender+"/body-se-walk-"+this.avatarAt+".png\">";
 				} else {
-					a_body.innerHTML = "<img src=\""+AvatarEngine.path+"assets/body/"+this.avatarGender+"/body-se-stand.png\">";
-
+					// not walking, so we just show the stand asset 
+					eBody.innerHTML = "<img src=\""+AvatarEngine.path+"assets/body/"+this.avatarGender+"/body-se-stand.png\">";
 				}
-
 			}
-			return a_body;
+			
+			return eBody;
 		}
 		
+		/**
+		 * generateLegs()
+		 * Returns x-avatar-legs 
+		 */
 		avatarElement.generateLegs = function() {
 			if(avatarElement.clothes.legs == 0 || avatarElement.clothes.legs == "0") {
 				return 0;
 			}
-			var a_body = document.createElement("x-avatar-legs");
-			a_body.style.position = "absolute";
-			a_body.style.zIndex = "12";
+			
+			// same deal with body, ill re-document it later
+			
+			var eLegs = document.createElement("x-avatar-legs");
+			eLegs.style.position = "absolute";
+			eLegs.style.zIndex = "12";
 			if(this.isSitting) {
-				a_body.innerHTML = "<img src=\""+AvatarEngine.path+"assets/legs/"+avatarElement.clothes.legs+"/legs-se-sit.png\">";
+				eLegs.innerHTML = "<img src=\""+AvatarEngine.path+"assets/legs/"+avatarElement.clothes.legs+"/legs-se-sit.png\">";
 			} else {
 				if(this.isWalking) {
-					a_body.innerHTML = "<img src=\""+AvatarEngine.path+"assets/legs/"+avatarElement.clothes.legs+"/legs-se-walk-"+this.avatarAt+".png\">";
+					eLegs.innerHTML = "<img src=\""+AvatarEngine.path+"assets/legs/"+avatarElement.clothes.legs+"/legs-se-walk-"+this.avatarAt+".png\">";
 				} else {
-					a_body.innerHTML = "<img src=\""+AvatarEngine.path+"assets/legs/"+avatarElement.clothes.legs+"/legs-se-stand.png\">";
+					eLegs.innerHTML = "<img src=\""+AvatarEngine.path+"assets/legs/"+avatarElement.clothes.legs+"/legs-se-stand.png\">";
 				}				
 			}
 			
-			return a_body;
+			return eLegs;
 		}
+		
+		// We call update every 150ms, this is only used for arm/leg movemnets at the moment
+		// but in the future it will also be used for eye blinks.
 		setInterval(function() {
 			avatarElement.update();
 		}, 150);
+		
+		// Return the avatar element
 		return avatarElement;
 		
 	}
